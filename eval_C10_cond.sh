d34b26ba3fc6e649f0ca603da9d1b0539d6d65a2
Abhishek Sinha <abhishek.sinha94@gmail.com>
2020-06-11 03:38:42 -0700

    adding files

---
+++ eval_C10_cond.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+python sample.py --shuffle --batch_size 64 --parallel --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 500 --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-4 --dataset C10 --data_root /atlas/u/tsong/data --G_ortho 0.0 --G_attn 0 --D_attn 0 --G_init N02 --D_init N02 --ema --use_ema --ema_start 1000 --start_eval 50 --test_every 5000 --save_every 2000 --num_best_copies 5 --num_save_copies 2 --loss_type hinge --seed 0 --sample_npz --load_weights copy1 --sample_num_npz 10000 --experiment_name $1
+
+python fid_tf.py cifar10_10k_stats.npz samples/$1/samples.npz
+
+
+
