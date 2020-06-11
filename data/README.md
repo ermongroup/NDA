db8454ae0c5dfe266da0099e7b0749debb3758b7
Abhishek Sinha <abhishek.sinha94@gmail.com>
2020-06-11 03:36:14 -0700

    adding files

---
+++ data/README.md
@@ -0,0 +1,1 @@
+data will be downloaded here
---
+++ train_C10_cond.sh
@@ -0,0 +1,2 @@
+python train_aug.py --shuffle --batch_size 64 --parallel --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 500 --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-4 --dataset C10 --data_root ./data/ --G_ortho 0.0 --G_attn 0 --D_attn 0 --G_init N02 --D_init N02 --ema --use_ema --ema_start 1000 --start_eval 50 --test_every 5000 --save_every 2000 --num_best_copies 5 --num_save_copies 2 --loss_type hinge --seed 2 --which_best FID --model BigGAN_new --alpha 0.25 --beta 0.75 --experiment_name jigsaw_C10 --conditional
+
